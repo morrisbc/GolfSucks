0cb660a41f94c3c8ed780ee331c1595399f03029
morrisbc <31737772+morrisbc@users.noreply.github.com>
2019-03-14 00:31:14 -0400

    Dashboard current nav option now stays highlighted. Added logo to dashboard that goes back to landing page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,35 @@
 function showNavMenu() {
-  var btns = document.getElementsByClassName("account");
+  let btns = document.getElementsByClassName("account");
 
   for (let btn of btns) {
     if (btn.className === "account") {
       btn.className += " responsive";
-    }
-    else {
+    } else {
       btn.className = "account";
+    }
+  }
+}
+
+var navOptions = document.querySelectorAll(".nav-elem");
+navOptions.forEach(function(option) {
+  option.addEventListener("click", highlightCurrent);
+});
+
+function highlightCurrent(e) {
+  let current;
+
+  if (e.target.classList.contains("nav-elem")) {
+    e.target.className += " nav-elem-current";
+    current = e.target;
+  } else if (e.target.parentElement.classList.contains("nav-elem")) {
+    e.target.parentElement.className += " nav-elem-current";
+    current = e.target.parentElement;
+  }
+
+  for (let option of navOptions) {
+    if (option !== current && option.classList.contains("nav-elem-current")) {
+      option.classList.remove("nav-elem-current");
+      break;
     }
   }
 }
